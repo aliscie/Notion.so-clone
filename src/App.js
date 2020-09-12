aa15f9a0913acc71d946e2f2f35d2f32bd150292
ali <weplutus.1@gmail.com>
2020-09-12 18:29:40 +0300

    drag adn drop but not keep

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      const elements = ['first', 'secon', 'thid ']
+      const grid = 8
+      return (
+            <DragDropContext onDragEnd={(r) => console.log(r)}>
+                  <Droppable droppableId='droppable'>
+                        {(provided, snapshot) => (
+                              <div
+                                    ref={provided.innerRef}
+                                    // {...provided.droppableProps}
+                                    style={{
+                                          background: provided.isDraggingOver ? 'lightblue' : 'grey',
+                                          padding: grid,
+                                          border: '5px solid pink',
+                                          width: '100%',
+                                          height: '1000px',
+                                          overflow: 'auto'
+                                    }}
+                              >
+                                    {elements.map((e, index) => (
+                                          <Draggable key={`${index}`} draggableId={`${index}`} index={index} >
+                                                {(provided, snapshot) => (
+                                                      <div
+                                                            ref={provided.innerRef}
+                                                            {...provided.draggableProps}
+                                                            {...provided.dragHandleProps}
+                                                            style={{
+                                                                  background: provided.isDraggingOver ? 'lightgreen' : 'green',
+                                                                  padding: grid,
+                                                                  border: '5px solid pink',
+                                                                  width: '100%',
+                                                                  height: '1000px',
+                                                                  overflow: 'auto',
+                                                                  ...provided.draggableProps.style,
+                                                                  width: '100px',
+                                                                  height: '100px'
+                                                            }}
+                                                      >{e}</div>
+                                                )}
+                                          </Draggable>
+                                    ))}
+                              </div>
+                        )}
+                  </Droppable>
+            </DragDropContext >
+      )
 }
 
-export default App;
+export default App
