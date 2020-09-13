983fb216ab22c1554a208ef288d4e957a1267e05
ali <weplutus.1@gmail.com>
2020-09-13 15:25:21 +0300

    component

--- src/App.js
+++ src/App.js
@@ -1,72 +1,8 @@
 import React from 'react';
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
-
+import DND from './components/DND'
 
 function App() {
       let elements = ['first', 'secon', 'thid ']
-
-      const grid = 8
-      function Drag(elements, boxStyle, elementsStyle) {
-            return (
-                  <div>
-                        {elements.map((e, index) => (
-                              <Draggable key={`${index}`} draggableId={`${index}`} index={index} >
-                                    {(provided, snapshot) => (
-                                          <div
-                                                ref={provided.innerRef}
-                                                {...provided.draggableProps}
-                                                {...provided.dragHandleProps}
-                                                style={{
-                                                      background: provided.isDraggingOver ? 'lightgreen' : 'green',
-                                                      padding: grid,
-                                                      overflow: 'auto',
-                                                      ...provided.draggableProps.style,
-                                                      ...elementsStyle
-                                                }}
-                                          >{e}</div>
-                                    )}
-                              </Draggable>
-                        ))}
-                  </div>
-            )
-      }
-      function Drop(elements, boxStyle, elementsStyle) {
-            return (
-                  <Droppable droppableId='droppable'>
-                        {(provided, snapshot) => (
-                              <div
-                                    ref={provided.innerRef}
-                                    style={{
-                                          padding: grid,
-                                          overflow: 'auto',
-                                          height: '300px',
-                                          ...boxStyle
-                                    }}
-                              >
-                                    {Drag(elements, boxStyle, elementsStyle)}
-                                    {provided.placeholder}
-                              </div>
-                        )}
-                  </Droppable>
-            )
-      }
-      function DND({ elements, boxStyle, elementsStyle }) {
-
-            function reOrder(r) {
-                  // console.log(Array.from(elements))
-                  // console.log(elements)
-                  // console.log(elements, r.source.index, r.destination.index)
-                  const [removed] = elements.splice(r.source.index, 1);
-                  elements.splice(r.destination.index, 0, removed);
-            }
-
-            return (
-                  <DragDropContext onDragEnd={reOrder}>
-                        {Drop(elements, boxStyle, elementsStyle)}
-                  </DragDropContext >
-            )
-      }
-
       return (
             <div>
                   <DND boxStyle={{ background: 'black' }} elementsStyle={{ background: 'white' }} elements={elements} />
---
+++ src/components/DND.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
+
+const grid = 8
+function Drag(elements, boxStyle, elementsStyle) {
+      return (
+            <div>
+                  {elements.map((e, index) => (
+                        <Draggable key={`${index}`} draggableId={`${index}`} index={index} >
+                              {(provided, snapshot) => (
+                                    <div
+                                          ref={provided.innerRef}
+                                          {...provided.draggableProps}
+                                          {...provided.dragHandleProps}
+                                          style={{
+                                                background: provided.isDraggingOver ? 'lightgreen' : 'green',
+                                                padding: grid,
+                                                overflow: 'auto',
+                                                ...provided.draggableProps.style,
+                                                ...elementsStyle
+                                          }}
+                                    >{e}</div>
+                              )}
+                        </Draggable>
+                  ))}
+            </div>
+      )
+}
+function Drop(elements, boxStyle, elementsStyle) {
+      return (
+            <Droppable droppableId='droppable'>
+                  {(provided, snapshot) => (
+                        <div
+                              ref={provided.innerRef}
+                              style={{
+                                    padding: grid,
+                                    overflow: 'auto',
+                                    height: '300px',
+                                    ...boxStyle
+                              }}
+                        >
+                              {Drag(elements, boxStyle, elementsStyle)}
+                              {provided.placeholder}
+                        </div>
+                  )}
+            </Droppable>
+      )
+}
+function DND({ elements, boxStyle, elementsStyle }) {
+
+      function reOrder(r) {
+            const [removed] = elements.splice(r.source.index, 1);
+            elements.splice(r.destination.index, 0, removed);
+      }
+
+      return (
+            <DragDropContext onDragEnd={reOrder}>
+                  {Drop(elements, boxStyle, elementsStyle)}
+            </DragDropContext >
+      )
+}
+export default DND
