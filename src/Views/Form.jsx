ace8e5df9e291dd1d4b432fdeed7e0299f83b92f
ali <weplutus.1@gmail.com>
2020-09-27 15:38:44 +0300

    update from form

--- src/Views/Form.jsx
+++ src/Views/Form.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Form({ dragable, state, set, e }) {
 
+
       function handlKeyUp(event) {
-            // const index = state.findIndex((item) => `${item.row}` == event.target.id);
-            // console.log(event.target.id)
-            // state.findIndex((item) => console.log(item.row));
+            const index = state[0].row.findIndex((item) => `${item}` == event.target.placeholder);
+            set(pre => {
+                  //must update the alues base on the id.
+                  pre[state.length - 1].row[index] = event.target.value
+                  return pre
+            })
 
       }
       return (
